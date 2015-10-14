e37e340873f88e4c4f337f3235d54c2348ac2d36
Brad Habenicht <brad.habenicht@gmail.com>
2015-10-14 00:12:20 -0400

    add factory for transferring data between controllers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,21 +2,32 @@
 
 /* Controllers */
 
-var ticTacToeControllers = angular.module('ticTacToeControllers', ['playerService']);
+var ticTacToeControllers = angular.module('ticTacToeControllers', []);
 
-ticTacToeControllers.controller('StartController', ['$scope', 'playerService',
-    function($scope, playerService) {
-      $scope.player1Name = playerService.player1Name;
-      $scope.player2Name = playerService.player2Name;
-      $scope.player1Wins = 0;
-      $scope.player2Wins = 0;
+ticTacToeControllers.controller('StartController', ['$scope', 'Data',
+    function($scope, Data) {
+
+      $scope.$watch('player1Name', function (newVal, oldVal) {
+        if (newVal !== oldVal) Data.setPlayer1Name(newVal);
+      });
+ 
+      $scope.$watch('player2Name', function (newVal, oldVal) {
+        if (newVal !== oldVal) Data.setPlayer2Name(newVal);
+      });
     }]);
 
-ticTacToeControllers.controller('GameController', ['$scope', 'playerService',
-    function($scope, playerService) {
-        $scope.player1Name = playerService.player1Name;
-        $scope.player2Name = playerService.player2Name;
-       
+ticTacToeControllers.controller('GameController', ['$scope', 'Data',
+    function($scope, Data) {
+      $scope.player1Name = Data.getPlayer1Name();
+      $scope.player2Name = Data.getPlayer2Name();
+
+      $scope.$watch(function () { return Data.getPlayer1Name(); }, function (newVal, oldVal) {
+        if (newVal !== oldVal) $scope.player1Name = newVal;
+      });
+ 
+      $scope.$watch(function () { return Data.getPlayer2Name(); }, function (newVal, oldVal) {
+        if (newVal !== oldVal) $scope.player2Name = newVal;
+      });
     }]);
 
 ticTacToeControllers.controller('ResultsController', ['$scope', 'playerService',
--- app/js/factories.js
+++ app/js/factories.js
@@ -1,13 +1,24 @@
 
-myApp.factory('InboxFactory', function InboxFactory ($http) {
-  var exports = {};
-
-  exports.getMessages = function () {
-    return $http.get('json/emails.json')
-      .error(function (data) {
-        console.log('There was an error!', data);  
-      });
+myTicTacToe.factory('Data', function() {
+  var data = {
+     player1Name: '',
+     player2Name: ''
   };
 
-  return exports;
+  return {
+     getPlayer1Name: function () {
+       return data.player1Name;
+     },
+     getPlayer2Name: function () {
+       return data.player2Name;
+     },
+     setPlayer1Name: function (name) {
+       data.player1Name = name;
+     },
+     setPlayer2Name: function (name) {
+       data.player2Name = name;
+     }
+  }
 });
+
+
